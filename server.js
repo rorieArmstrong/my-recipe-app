d51d4741b742ee78e3995034bfa97d56bc7fb148
rorieArmstrong <rorie.armstrong@gmail.com>
2020-05-14 10:56:38 +0100

    use routes

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || 8000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
 var routes = require('./routes/index');
-
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use('/api', routes);
 
 app.listen(port);
 console.log("listening on potrt: "+port)
