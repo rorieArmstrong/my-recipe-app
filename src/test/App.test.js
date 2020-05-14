14afaa55a41cfe2cfe329fec9c974d12d7660a30
rorieArmstrong <rorie.armstrong@gmail.com>
2020-05-14 16:47:32 +0100

    add tests

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
+import Navbar from '../components/Navbar';
+import { Route } from 'react-router-dom';
+import { shallow, mount } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  test('contains Routes', () => {
+    expect(wrapper.containsMatchingElement(<Route />)).toEqual(true);
+  });
+
+  test('contains 4 Routes', () => {
+    expect((wrapper.find('Route').length)).toEqual(4);
+  });
+
+  it('Route to HomePage is exact', () => {     
+    let wrap = mount(<App />);                                 
+    let route = wrap.find(<Route path='/' exact/>);
+    expect(route).toBeTruthy();
+   });
+
 });
