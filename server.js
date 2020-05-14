9e413709efe6849c98dea690f9cfa5dc1a1e96af
rorieArmstrong <rorie.armstrong@gmail.com>
2020-05-14 10:10:05 +0100

    add queries

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
+
+var routes = require('./routes/index');
 
 app.get('/ping', function (req, res) {
  return res.send('pong');
